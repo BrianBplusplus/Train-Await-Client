5ae6594b3e28d894147ce80b9db91ce85b2db27a
BrianBplusplus <brian@nieuwenhuijzen.net>
2020-03-03 15:25:46 +0100

    initiated the Redux store

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return <div>Hola</div>;
+import { Route } from "react-router-dom";
+
+export class App extends Component {
+  render() {
+    return <div>Hola</div>;
+  }
 }
 
 export default App;
