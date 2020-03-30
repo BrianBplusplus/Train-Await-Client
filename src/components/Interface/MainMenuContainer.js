1118b50de6bcada41b51819285faf210ec886314
BrianBplusplus <brian@nieuwenhuijzen.net>
2020-03-30 16:37:52 +0200

    Added a how to play to the startscreen and added some basic styling

--- src/components/Interface/MainMenuContainer.js
+++ src/components/Interface/MainMenuContainer.js
@@ -6,9 +6,25 @@ export class MainMenuContainer extends Component {
   render() {
     return (
       <div>
-        <h1>Main menu</h1>
+        <h1>
+          <span className="titleLeft">train</span> <span className="titleRight">await</span>
+        </h1>
         <p>
-          <Link to="/game">Play Game</Link>
+          <span className="highlightText">How to Play:</span>
+          <br></br>
+          Rotate the tracks by clicking on them, once all the tracks are in place press <span className="highlightText">"Play!"</span> to make the
+          train move
+          <p>
+            <span className="highlightText">Scoring:</span>
+            <br></br>
+            The longer the train rides the <span className="highlightText">higher</span> your score <br></br>
+            The amount of times you rotate the train tracks <span className="highlightText">reduce</span> your score
+          </p>
+        </p>
+        <p>
+          <Link className="titleRight" to="/game">
+            Play Game
+          </Link>
         </p>
       </div>
     );
